719cfbe63e748f4531f40685892bbf7aa61e6339
Michael Kantor <michael@layer.com>
2017-08-14 11:23:22 -0700

    Version increment

--- src/client.js
+++ src/client.js
@@ -640,7 +640,7 @@ Client.prototype.telemetryMonitor = null;
  * @static
  * @type {String}
  */
-Client.version = '3.4.0';
+Client.version = '3.4.1';
 
 /**
  * Any  Message that is part of a Query's results are kept in memory for as long as it
